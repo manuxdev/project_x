a51e97d610924b8daa88dbd192daa7badc0a4888
Manuel Pantoja <manuxdev1010@gmail.com>
2024-03-05 17:59:11 -0500

    base
    nav-hero-about

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode:'class',
+	theme: {
+		extend: {
+			colors:{
+				'palete-blue-dark':"#042653",
+				'palete-blue-light':"#327ED5",
+				'palete-white-blue':"#BEC8DA",
+				'palete-white':"#FFFF",
+			},
+			
+		},
+	},
+	plugins: [],
+}
