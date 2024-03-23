08684270253880c3827b98836ed6570156cd5be9
Manuel Pantoja <manuxdev1010@gmail.com>
2024-03-23 18:54:44 -0400

    i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,46 @@
+export const languages = {
+    en: 'en',
+    es: 'es',
+    rs:'rs'
+  };
+  
+  export const defaultLang = 'en';
+  
+  export const ui = {
+    en: {
+            'nav.places': 'Places',
+            'nav.about': 'About',
+            'nav.services': 'Services',
+            'nav.feedback': 'FeedBack',
+            'hero.h1':'EASY WAY TO',
+            'hero.h1-2':'THE CITY',
+            'hero.span':'EXPLORE',
+            'hero.p':'Experience the reliability, efficiency and quality that only our taxi service can offer. Driven by highly qualified professionals, we guarantee a safe, punctual and comfortable trip.',
+
+    },
+    es: {
+   
+            'nav.places': 'Destinos',
+            'nav.about': 'Sobre Nosotros',
+            'nav.services': 'Servicios',
+            'nav.feedback': 'Comentarios',
+            'hero.h1':'CAMINO FÁCIL A',
+            'hero.h1-2':'LA CIUDAD',
+            'hero.span':'EXPLORAR',
+            'hero.p':"Experimenta la confiabilidad, eficiencia y calidad que solo nuestro servicio de taxis puede ofrecer. Conducidos por profesionales altamente calificados, garantizamos un viaje seguro, puntual y cómodo."
+        },
+     
+  
+    rs: {
+       
+            'nav.places': 'Направления',
+            'nav.about': 'О нас',
+            'nav.services': 'Услуги',
+            'nav.feedback': 'Комментарии',
+            'hero.h1':'ЛЕГКИЙ ПУТЬ К',
+            'hero.h1-2':'ГОРОД',
+            'hero.span':'ИССЛЕДОВАТЬ',
+            'hero.p':"Почувствуйте надежность, эффективность и качество, которые может предложить только наша служба такси. Под руководством высококвалифицированных специалистов мы гарантируем безопасную, пунктуальную и комфортную поездку."
+     
+    },
+  } as const;
